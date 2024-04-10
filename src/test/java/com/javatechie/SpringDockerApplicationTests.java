0df26aef648580532e1a919c1039da27676b9f05
kavitabora <kavitabora9211@gmail.com>
2024-04-10 10:27:58 +0530

    test case added

--- src/test/java/com/javatechie/SpringDockerApplicationTests.java
+++ src/test/java/com/javatechie/SpringDockerApplicationTests.java
@@ -1,13 +1,23 @@
 package com.javatechie;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@WebMvcTest
 class SpringDockerApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testGreetingEndpoint() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/greeting"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("welcome to java docker development"));
 	}
 
 }
